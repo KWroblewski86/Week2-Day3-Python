c213ddf9418f7ffd1a59cf1c4725305bdb376465
Kyle Wroblewski <kyle.a.wroblewski@gmail.com>
2022-09-29 15:29:54 -0500

    first commit

---
+++ Modules/module.py
@@ -0,0 +1,21 @@
+def square_footage(length, width):
+    return length * width
+
+print(square_footage(300, 100))
+print(square_footage(500, 200))
+print(square_footage(288, 50))
+print(square_footage(250, 150))
+print(square_footage(215, 150))
+print(square_footage(125, 501))
+
+
+
+def circumference(pi=3.14, radius=''):
+    circle = 2 * pi * radius
+    return circle
+
+print(circumference(radius=5))
+print(circumference(radius=3))
+print(circumference(radius=6))
+print(circumference(radius=8))
+    
